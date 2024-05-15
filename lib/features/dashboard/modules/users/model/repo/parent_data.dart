0471a06f2dca31ca47ae4ec2e74481f5278342da
yousef-jpg <yousefkhairyy@gmail.com>
2024-05-16 02:23:36 +0300

    dashbord

--- lib/features/dashboard/modules/users/model/repo/parent_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mtask/features/dashboard/modules/users/model/user_model.dart';
-
-abstract class ParentRepo {
-  Future<void> insert({required String name, String? email});
-  Future<List<UserModel>> fetch();
-  Future<void> delete({required int id});
-}
